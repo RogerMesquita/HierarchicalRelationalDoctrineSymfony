7e7e9dcbd2bbaf494010bb307381c93e0475cb5a
RogerMesquita <rogermesquitaa@hotmail.com>
2021-01-18 10:20:13 -0300

    Start Commit

---
+++ src/Entity/Category.php
@@ -0,0 +1,125 @@
+<?php
+
+
+namespace App\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+use Doctrine\ORM\Mapping\Index;
+
+/**
+ * Class Category
+ * @package App\Entity
+ * @Gedmo\Tree(type="nested")
+ * @ORM\Table(
+ *     name="category",
+ *     indexes={
+ *          @Index(name="lft_ix", columns={"lft"}),
+ *          @Index(name="rgt_ix", columns={"rgt"}),
+ *          @Index(name="lvl_ix", columns={"lvl"})
+ *     })
+ * @ORM\Entity(repositoryClass="Gedmo\Tree\Entity\Repository\NestedTreeRepository")
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @Gedmo\TreeLeft()
+     * @ORM\Column(type="integer")
+     */
+    private $lft;
+
+    /**
+     * @Gedmo\TreeLevel()
+     * @ORM\Column(type="integer")
+     */
+    private $lvl;
+
+    /**
+     * @Gedmo\TreeRight()
+     * @ORM\Column(type="integer")
+     */
+    private $rgt;
+
+    /**
+     * @Gedmo\TreeRoot()
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+     * @ORM\JoinColumn(name="tree_root", referencedColumnName="id", onDelete="cascade")
+     */
+    private $root;
+
+    /**
+     * @Gedmo\TreeParent()
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", onDelete="cascade")
+     */
+    private $parent;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Category", mappedBy="parent")
+     * @ORM\OrderBy({"lft" = "ASC"})
+     */
+    private $children;
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return Category
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @param Category $parent
+     */
+    public function setParent($parent): void
+    {
+        $this->parent = $parent;
+    }
+
+    /**
+     * @return Category
+     */
+    public function getRoot()
+    {
+        return $this->root;
+    }
+
+}
